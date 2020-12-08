6f03010e270f779b144d2a44b524288a3285ee00
Mahdi <m.rezig@enano-tech.com>
2020-12-08 09:44:58 +0100

    -

--- driveworks.php
+++ driveworks.php
@@ -7,3 +7,24 @@
 * Author: driveworks
 * Author URI: http://driveworks.com/
 **/
+
+
+header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
+header('Pragma: no-cache'); // HTTP 1.0.
+header('Expires: 0'); // Proxies.
+
+add_action( 'rest_api_init', 'my_register_route' );
+
+function my_register_route() {
+
+    register_rest_route( 'drive_works', 'get_order', array(
+            'methods' => 'POST',
+            'callback' => 'custom_phrase',
+        )
+    );
+}
+
+function custom_phrase() {
+    //:: @houssem todo get product_name product_description product_img( url or base64) , create media , ceate ecommerce_product
+}
+
